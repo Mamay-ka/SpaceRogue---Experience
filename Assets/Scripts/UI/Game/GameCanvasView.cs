3531cbbca1aab5bfe24688d2365ab5991acfb44f
Mamay-ka <mamay_ka@mail.ru>
2023-03-05 21:57:18 +0300

    Experience
    05/03/23

---
+++ Assets/Scripts/UI/Game/GameCanvasView.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace UI.Game
+{
+    public sealed class GameCanvasView : MonoBehaviour
+    {
+        [field: SerializeField] public Transform PlayerInfo { get; private set; }
+        [field: SerializeField] public Transform EnemyHealthBars { get; private set; }
+        [field: SerializeField] public Transform GameEventIndicators { get; private set; }
+        [field: SerializeField] public Transform LevelInfo { get; private set; }
+
+        [field: SerializeField] public Transform ExperienceBar { get; private set; }//!!!!
+    }
+}
